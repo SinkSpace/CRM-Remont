be1494c8b458f7f6082b120889ade8f8e766c819
SinkSpace <vtamaichuk@yandex.ru>
2026-06-01 00:11:25 +0500

    1.1.1: database name change

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const { Pool } = require("pg");
 const pool = new Pool({
     user: "crmuser",
     host: "localhost",
-    database: "crm",
+    database: "postgres",
     password: "123456789",
     port: 5432
 });
