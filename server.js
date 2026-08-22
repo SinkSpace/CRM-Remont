5cc503a83c638fd27d5e75a4b63fff1f21e99fdb
SinkSpace <vtamaichuk@yandex.ru>
2026-08-22 19:34:03 +0500

    fix

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
-const greenlock = require('greenlock-express');
+const Greenlock = require('greenlock');
+const express = require('express');
 
-greenlock.create({
+const app = require('./app');
+
+const greenlock = Greenlock.create({
   server: 'https://acme-v02.api.letsencrypt.org/directory',
   version: 'draft-11',
-
+  
   email: 'stepnoywolk111@gmail.com',
   approveDomains: ['crmsink.ru', 'www.crmsink.ru'],
   agreeTos: true,
   
   configDir: './acme/',
   
-  app: require('./app')
-}).listen(80, 443);
+  httpsOptions: {
+
+  }
+});
+
+greenlock.listen(80, 443, function() {
+  console.log('HTTPS сервер запущен на портах 80 и 443');
+});
+
+greenlock.serveApp(app);
