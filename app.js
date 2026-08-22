041ca894525849e30176a7c7f466e684fb04dabe
SinkSpace <vtamaichuk@yandex.ru>
2026-08-22 19:26:07 +0500

    https hosting

---
+++ app.js
@@ -0,0 +1,14 @@
+require('greenlock-express').create({
+  server: 'https://acme-v02.api.letsencrypt.org/directory',
+  version: 'draft-11',
+
+  email: 'stepnoywolk111@gmail.com',
+  approveDomains: ['crmsink.ru', 'www.crmsink.ru'],
+  agreeTos: true,
+
+  configDir: './acme/',
+
+  app: require('./app.js')
+}).listen(80, 443);
+
+/* */
