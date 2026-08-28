489405567a07a5a887a4e75dcb27f0170fac9d81
SinkSpace <vtamaichuk@yandex.ru>
2026-08-29 01:14:49 +0500

    MVC #2.4.5

--- models/settingsModels.js
+++ models/settingsModels.js
@@ -2,7 +2,7 @@ const express = require('express');
 const pool = require('../db');
 const client = pool.connect();
 
-const registrationEnabled = async (data) => {
+async function registrationEnabled(data) {
     return await client.query(
         'SELECT registration_enabled FROM system_settings WHERE id = 1'
     );
