b03b585946eb986bd4091c45aa594b11c1362db2
SinkSpace <vtamaichuk@yandex.ru>
2026-09-06 13:35:45 +0500

    MVC #3.0.10

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const http = require('http');
 const fs = require('fs');
 const app = require('./app');
 
+require('dotenv').config();
 const domain = process.env.DOMAIN || 'localhost';
+
 const options = {
   key: fs.readFileSync(`/etc/letsencrypt/live/${domain}/privkey.pem`),
   cert: fs.readFileSync(`/etc/letsencrypt/live/${domain}/fullchain.pem`)
