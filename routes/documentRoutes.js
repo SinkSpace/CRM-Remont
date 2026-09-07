6e4ba71739e845599ed3a8be7b0017818eca3e9c
SinkSpace <vtamaichuk@yandex.ru>
2026-09-07 13:17:44 +0500

    MVC #3.0.20

--- routes/documentRoutes.js
+++ routes/documentRoutes.js
@@ -1,7 +1,23 @@
 const get = require('../controllers/documentController');
+const multer = require('multer');
+const path = require('path');
 const express = require('express');
 const router = express.Router();
-const upload = require('../middleware/middleware');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, '..', 'uploads', 'templates'));
+    },
+
+    filename: (req, file, cb) => {
+        const ext = path.extname(file.originalname);
+        const filename = `${Date.now()}-${Math.round(Math.random() * 1e9)}${ext}`;
+
+        cb(null, filename);
+    }
+});
+
+const upload = multer({ storage });
 
 /* 9.1 Получение */
 
